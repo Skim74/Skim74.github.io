2784b2c4184b22c2b076bbfba29d4b9e9923f4b3
Sarah Miller <SMiller2016@u.northwestern.edu>
2017-09-19 13:55:01 -0700

    quotes updates

--- RandoQuotes/quotes.js
+++ RandoQuotes/quotes.js
@@ -1,30 +1,38 @@
 const url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
-$(document).ready(function() {
-  $.ajaxSetup({
-    cache: false
-  });
-  $("#get-quote").on("click", getQuote)
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("get-quote").addEventListener("click", noJQueryQuote);
 });
 
-function getQuote() {
+function noJQueryQuote() {
   let link;
-  $.getJSON(url, function(a) {
-    if (a[0].content.length>140) {
-      getQuote();
-    } else {
-      if (a[0].custom_meta) {
-        let longlink = a[0].custom_meta.Source;
-        link = longlink.substr(0, longlink.indexOf('>') + 1);
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `${url}&t=${new Date().getTime()}`, true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      var data = JSON.parse(this.response);
+      if (data[0].content.length > 140) {
+        noJQueryQuote();
       } else {
-        link = `<a href="${a[0].link}">`;
+        if (data[0].custom_meta) {
+          let longlink = data[0].custom_meta.Source;
+          link = longlink.substr(0, longlink.indexOf('>') + 1);
+        } else {
+          link = `<a href="${data[0].link}">`;
+        }
+        document.getElementById("quote-text").innerHTML = data[0].content;
+        document.getElementById("quote-author").innerHTML = `${link} &mdash; ${data[0].title} </a>`;
+        document.getElementById("get-quote").innerHTML = 'New Quote';
+        document.body.style.background = `#${Math.floor(Math.random()*16777215).toString(16)}`;
       }
-      $("#quote-text").html(`${a[0].content}`);
-      $("#quote-author").html(`${link} &mdash; ${a[0].title} </a>`);
-      $("#get-quote").text(`New Quote`).removeClass("unclicked");
-      $("body").css('background', `#${Math.floor(Math.random()*16777215).toString(16)}`);
+    } else {
+      console.log('ruh rho');
     }
-  });
-
-
+  };
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+  request.send();
 }
---
+++ app/index.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var QuoteBox = require('./components/QuoteBox');
+
+ReactDOM.render(
+  <QuoteBox />,
+  document.getElementById('quote-box')
+);
+ReactDOM.render(
+  <QuoteBox />,
+  document.getElementById('quote-box1')
+);
+
+ReactDOM.render(
+  <QuoteBox />,
+  document.getElementById('quote-box2')
+);
